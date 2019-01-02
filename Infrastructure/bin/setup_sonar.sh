8f78691f40fd73cae41249c4866f435a30e1c27b
Singh Joginder (Supplier) <joginder.singh@supplier.volvo.com>
2019-01-02 16:12:36 +0000

    Updated Scripts2

--- Infrastructure/bin/setup_sonar.sh
+++ Infrastructure/bin/setup_sonar.sh
@@ -14,3 +14,21 @@ echo "Setting up Sonarqube in project $GUID-sonarqube"
 # oc new-app -f ../templates/sonarqube.yaml --param .....
 
 # To be Implemented by Student
+
+oc project $GUID-sonarqube 
+
+sleep 60
+
+oc process -f Infrastructure/templates/template-sonarqube.yml -n ${GUID}-sonarqube -p GUID=${GUID} | oc create -n ${GUID}-sonarqube -f -
+
+while : ; do
+    echo "Checking if Sonarqube is Ready..."
+    oc get pod -n ${GUID}-sonarqube | grep "sonarqube" | grep -v deploy | grep "1/1"
+    if [ $? == "1" ] 
+      then 
+      echo "Waiting 10 seconds..."
+        sleep 10
+      else 
+        break 
+    fi
+done
